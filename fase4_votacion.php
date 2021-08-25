c803d004626851ba5910b70c859acb35920f6005
Hugo Andres Orozco <hugandoro@gmail.com>
2021-08-25 12:36:13 -0500

    Update fase4_votacion.php

--- fase4_votacion.php
+++ fase4_votacion.php
@@ -184,6 +184,14 @@ require_once('bd/sle.php');
             <img src="images/fotos_votacion/F20.jpeg" width="100%">
             </div>            
         </div>
+        
+        <hr><br>
+
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-xs-12" align="center">
+                <iframe width="100%" height="520" src="https://www.youtube.com/embed/i9_nyHxprsA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
 
         <hr><br>
 
